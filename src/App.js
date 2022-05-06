6c649fdfd73a8869f63cc72f8320e788665cef3a
Hristijan Krstev <hristijankrstev@Hristijans-MacBook-Air.local>
2022-05-05 21:51:12 -0500

    modified src/App.js to include my name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hristijan Krstev - Project 4
+          Hristijan Krstev - Macedonia
         </p>
         <a
           className="App-link"
